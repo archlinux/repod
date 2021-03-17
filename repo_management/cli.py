d542d1f4388ca1d2c79046a08a4a98a6ae91cafc
David Runge <dvzrv@archlinux.org>
2021-03-18 00:05:22 +0100

    Add entrypoint for json2db
    repo_management/cli.py:
    Add `json2db()` as entrypoint for the `json2db` script.
    
    tests/test_cli.py:
    Add a test for `json2db()`.

--- repo_management/cli.py
+++ repo_management/cli.py
@@ -1,4 +1,4 @@
-from repo_management import argparse, operations
+from repo_management import argparse, defaults, operations
 
 
 def db2json() -> None:
@@ -12,4 +12,19 @@ def db2json() -> None:
     operations.dump_db_to_json_files(
         input_path=args.db_file,
         output_path=args.output_dir,
+    )
+
+
+def json2db() -> None:
+    """The entry point for the json2db script
+
+    The method calls operations.create_db_from_json_files() which creates a repository database from a set of JSON files
+    in a directory.
+    """
+
+    args = argparse.ArgParseFactory.json2db().parse_args()
+    operations.create_db_from_json_files(
+        input_path=args.input_dir,
+        output_path=args.db_file,
+        db_type=defaults.RepoDbType.FILES if args.files else defaults.RepoDbType.DEFAULT,
     )
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,8 +1,9 @@
 from argparse import Namespace
 
 from mock import Mock, patch
+from pytest import mark
 
-from repo_management import cli
+from repo_management import cli, defaults
 
 
 @patch("repo_management.operations.dump_db_to_json_files")
@@ -12,3 +13,19 @@ def test_db2json(argparsefactory_mock: Mock, dump_db_to_json_files_mock: Mock) -
     argparsefactory_mock.db2json.return_value = Mock(parse_args=Mock(return_value=namespace))
     cli.db2json()
     dump_db_to_json_files_mock.assert_called_once_with(input_path=namespace.db_file, output_path=namespace.output_dir)
+
+
+@mark.parametrize("files, db_type", [(True, defaults.RepoDbType.FILES), (False, defaults.RepoDbType.DEFAULT)])
+@patch("repo_management.operations.create_db_from_json_files")
+@patch("repo_management.argparse.ArgParseFactory")
+def test_json2db(
+    argparsefactory_mock: Mock, create_db_from_json_files: Mock, files: bool, db_type: defaults.RepoDbType
+) -> None:
+    namespace = Namespace(db_file="db_file", input_dir="input_dir", files=files)
+    argparsefactory_mock.json2db.return_value = Mock(parse_args=Mock(return_value=namespace))
+    cli.json2db()
+    create_db_from_json_files.assert_called_once_with(
+        input_path=namespace.input_dir,
+        output_path=namespace.db_file,
+        db_type=db_type,
+    )
