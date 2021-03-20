aaf40e0233da598621c18674c9bf8e3c11ed036a
David Runge <dvzrv@archlinux.org>
2021-03-20 13:23:48 +0100

    Extend tests for CLI and add integration tests
    repo_management/cli.py:
    Change `db2json` and `json2db` to try/except
    `errors.RepoManagementError` and `argpase.ArgumentTypeError` and exit
    with a non-zero return code in that case.
    
    tests/test_cli.py:
    Change the tests for `db2json` and `json2db` to cover potentially
    raising `errors.RepoManagementError` and `argpase.ArgumentTypeError` and
    checking whether exiting with a non-zero return code in that case.
    Add parametrized integration test `test_transform_databases()` that
    transforms existing pacman .files sync databases to JSON files and back
    to .files repository databases again.
    Add parametrized integration
    `test_transform_databases_and_use_with_pacman()` to transform existing
    pacman .files sync databases to JSON files and create .files and .db
    repository databases from them. Afterwards pacman uses the created
    repository databases to search for packages with the "linux" keyword in
    them and list the files owned by the "linux" package using the created
    .files sync databases.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,31 +1,196 @@
-from argparse import Namespace
+import shutil
+import tempfile
+from argparse import ArgumentTypeError, Namespace
+from pathlib import Path
+from typing import Dict, Iterator
 
 from mock import Mock, patch
-from pytest import mark
+from pytest import fixture, mark
 
-from repo_management import cli, defaults
+from repo_management import cli, commands, defaults, errors
 
 
+@fixture(scope="function")
+def empty_dir() -> Iterator[Path]:
+    directory = tempfile.mkdtemp()
+    yield Path(directory)
+    shutil.rmtree(directory)
+
+
+@fixture(scope="function")
+def empty_file() -> Iterator[Path]:
+    [foo, file_name] = tempfile.mkstemp()
+    yield Path(file_name)
+
+
+@mark.parametrize(
+    "fail_argparse, fail_dump",
+    [
+        (False, False),
+        (True, False),
+        (False, True),
+    ],
+)
 @patch("repo_management.operations.dump_db_to_json_files")
 @patch("repo_management.argparse.ArgParseFactory")
-def test_db2json(argparsefactory_mock: Mock, dump_db_to_json_files_mock: Mock) -> None:
+@patch("repo_management.cli.exit")
+def test_db2json(
+    exit_mock: Mock,
+    argparsefactory_mock: Mock,
+    dump_db_to_json_files_mock: Mock,
+    fail_argparse: bool,
+    fail_dump: bool,
+) -> None:
     namespace = Namespace(db_file="db_file", output_dir="output_dir")
-    argparsefactory_mock.db2json.return_value = Mock(parse_args=Mock(return_value=namespace))
+    if fail_argparse:
+        argparsefactory_mock.db2json.side_effect = ArgumentTypeError
+    else:
+        argparsefactory_mock.db2json.return_value = Mock(parse_args=Mock(return_value=namespace))
+    if fail_dump:
+        dump_db_to_json_files_mock.side_effect = errors.RepoManagementError
+
     cli.db2json()
-    dump_db_to_json_files_mock.assert_called_once_with(input_path=namespace.db_file, output_path=namespace.output_dir)
+    if fail_argparse or fail_dump:
+        exit_mock.assert_called_once_with(1)
+    else:
+        dump_db_to_json_files_mock.assert_called_once_with(
+            input_path=namespace.db_file,
+            output_path=namespace.output_dir,
+        )
 
 
-@mark.parametrize("files, db_type", [(True, defaults.RepoDbType.FILES), (False, defaults.RepoDbType.DEFAULT)])
+@mark.parametrize(
+    "files, db_type, fail_argparse, fail_create",
+    [
+        (True, defaults.RepoDbType.FILES, False, False),
+        (False, defaults.RepoDbType.DEFAULT, False, False),
+        (True, defaults.RepoDbType.FILES, True, False),
+        (False, defaults.RepoDbType.DEFAULT, True, False),
+        (True, defaults.RepoDbType.FILES, False, True),
+        (False, defaults.RepoDbType.DEFAULT, False, True),
+    ],
+)
 @patch("repo_management.operations.create_db_from_json_files")
 @patch("repo_management.argparse.ArgParseFactory")
+@patch("repo_management.cli.exit")
 def test_json2db(
-    argparsefactory_mock: Mock, create_db_from_json_files: Mock, files: bool, db_type: defaults.RepoDbType
+    exit_mock: Mock,
+    argparsefactory_mock: Mock,
+    create_db_from_json_files_mock: Mock,
+    files: bool,
+    db_type: defaults.RepoDbType,
+    fail_argparse: bool,
+    fail_create: bool,
 ) -> None:
     namespace = Namespace(db_file="db_file", input_dir="input_dir", files=files)
-    argparsefactory_mock.json2db.return_value = Mock(parse_args=Mock(return_value=namespace))
+    if fail_argparse:
+        argparsefactory_mock.json2db.side_effect = ArgumentTypeError
+    else:
+        argparsefactory_mock.json2db.return_value = Mock(parse_args=Mock(return_value=namespace))
+    if fail_create:
+        create_db_from_json_files_mock.side_effect = errors.RepoManagementError
+
     cli.json2db()
-    create_db_from_json_files.assert_called_once_with(
-        input_path=namespace.input_dir,
-        output_path=namespace.db_file,
-        db_type=db_type,
+    if fail_argparse:
+        exit_mock.assert_called_once_with(1)
+    if fail_create:
+        create_db_from_json_files_mock.assert_called_once_with(
+            input_path=namespace.input_dir,
+            output_path=namespace.db_file,
+            db_type=db_type,
+        )
+        exit_mock.assert_called_once_with(1)
+    if not fail_argparse and not fail_create:
+        create_db_from_json_files_mock.assert_called_once_with(
+            input_path=namespace.input_dir,
+            output_path=namespace.db_file,
+            db_type=db_type,
+        )
+
+
+@mark.integration
+@mark.parametrize(
+    "db",
+    [
+        ("core"),
+        ("extra"),
+        ("community"),
+    ],
+)
+def test_transform_databases(db: str, empty_dir: Path, empty_file: Path) -> None:
+    commands.run_command(
+        cmd=["db2json", f"/var/lib/pacman/sync/{db}.files", empty_dir.as_posix()],
+        debug=True,
+        check=True,
+    )
+    commands.run_command(
+        cmd=["json2db", "-f", empty_dir.as_posix(), empty_file.as_posix()],
+        debug=True,
+        check=True,
+    )
+
+
+@mark.integration
+def test_transform_databases_and_use_with_pacman(empty_dir: Path) -> None:
+    dbs = ["core", "extra", "community"]
+    json_paths: Dict[str, Path] = {}
+    pacman_path = Path(empty_dir / Path("pacman"))
+    pacman_path.mkdir(parents=True)
+    db_path = Path(empty_dir / Path("pacman/db_path"))
+    db_path.mkdir(parents=True)
+    sync_path = Path(empty_dir / Path("pacman/db_path/sync"))
+    sync_path.mkdir(parents=True)
+    cache_path = Path(empty_dir / Path("pacman/cache_path"))
+    cache_path.mkdir(parents=True)
+    for db in dbs:
+        json_path = Path(empty_dir / Path(db))
+        json_path.mkdir()
+        json_paths[db] = json_path
+
+    for (name, json_path) in json_paths.items():
+        commands.run_command(
+            cmd=["db2json", f"/var/lib/pacman/sync/{name}.files", json_path.as_posix()],
+            debug=True,
+            check=True,
+        )
+        commands.run_command(
+            cmd=["json2db", json_path.as_posix(), f"{sync_path.as_posix()}/{name}.db"],
+            debug=True,
+            check=True,
+        )
+        commands.run_command(
+            cmd=["json2db", "-f", json_path.as_posix(), f"{sync_path.as_posix()}/{name}.files"],
+            debug=True,
+            check=True,
+        )
+
+    commands.run_command(
+        cmd=[
+            "pacman",
+            "--cache",
+            cache_path.as_posix(),
+            "--logfile",
+            f"{pacman_path.as_posix()}/pacman.log",
+            "--dbpath",
+            db_path.as_posix(),
+            "-Ss",
+            "linux",
+        ],
+        debug=True,
+        check=True,
+    )
+    commands.run_command(
+        cmd=[
+            "pacman",
+            "--cache",
+            cache_path.as_posix(),
+            "--logfile",
+            f"{pacman_path.as_posix()}/pacman.log",
+            "--dbpath",
+            db_path.as_posix(),
+            "-Fl",
+            "linux",
+        ],
+        debug=True,
+        check=True,
     )
