9b31c32ed0d267891e6b4d2a3e8a6a7a592f3fd3
David Runge <dvzrv@archlinux.org>
2021-06-30 23:58:11 +0200

    Add defaults for configuration
    repo_management/defaults.py:
    Add defaults relevant for configuration, such as available/known
    architetures, settings location and settings override location as well
    as default locations for the package repositories, the source
    repositories, the management repository, the package and source pool.

--- repo_management/defaults.py
+++ repo_management/defaults.py
@@ -1,10 +1,20 @@
 from enum import IntEnum
+from pathlib import Path
 from typing import Dict, Union
 
 DB_USER = "root"
 DB_GROUP = "root"
 DB_FILE_MODE = "0644"
 DB_DIR_MODE = "0755"
+ARCHITECTURES = ["aarch64", "arm", "armv6h", "armv7h", "i486", "i686", "pentium4", "riscv32", "riscv64", "x86_64"]
+SETTINGS_LOCATION = Path("/etc/arch-repo-management/config.toml")
+SETTINGS_OVERRIDE_LOCATION = Path("/etc/arch-repo-management.d/")
+PACKAGE_REPO_BASE = Path("/var/lib/arch-repo-management/repo")
+SOURCE_REPO_BASE = Path("/var/lib/arch-repo-management/source")
+
+MANAGEMENT_REPO = Path("/var/lib/arch-repo-management/management/default")
+PACKAGE_POOL = Path("/var/lib/arch-repo-management/pool/package/default")
+SOURCE_POOL = Path("/var/lib/arch-repo-management/pool/source/default")
 
 
 class RepoDbMemberType(IntEnum):
