dc76b71e70c21adb8a94d1ce9e9e63dad7f594f7
Anton Hvornum <anton@hvornum.se>
2022-02-03 10:22:36 +0100

    Added a 'getting started' guide. It most certenly is wrong either as a whole concept, or parts of it. In which case, feel free to scrap - change - or point it out :)
    Revert "Added a 'getting started' guide. It most certenly is wrong either as a whole concept, or parts of it. In which case, feel free to scrap - change - or point it out :)"
    
    This reverts commit 53cc3b10505abb18c95d33ef3431d440bd6e7530.
    
    This should disable any potential use case where XSS could be a problem: https://jinja.palletsprojects.com/en/3.0.x/api/#autoescaping
    
    Fixed black complaints

--- repo_management/convert.py
+++ repo_management/convert.py
@@ -172,6 +172,7 @@ class RepoDbFile:
             trim_blocks=True,
             lstrip_blocks=True,
             enable_async=enable_async,
+            autoescape=True,
         )
 
     async def render_desc_template(self, model: models.PackageDesc, output: io.StringIO) -> None:
