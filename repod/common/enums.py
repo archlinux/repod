5657bc553312c22d580f54426c556258db9d0a60
David Runge <dvzrv@archlinux.org>
2022-05-29 00:35:32 +0200

    Add common enum for compression types
    repod/common/enums.py:
    Add `CompressionTypeEnum` for distinguishing tar compression types.
    Add `tar_compression_types_for_filename_regex()` to provide the values
    of CompressionTypeEnum members in the context of regular expressions.

--- repod/common/enums.py
+++ repod/common/enums.py
@@ -1,4 +1,4 @@
-from enum import IntEnum
+from enum import Enum, IntEnum
 
 
 class FieldTypeEnum(IntEnum):
@@ -17,3 +17,40 @@ class FieldTypeEnum(IntEnum):
     STRING = 0
     INT = 1
     STRING_LIST = 2
+
+
+class CompressionTypeEnum(Enum):
+    """An IntEnum to distinguish different compression types of a tar file
+
+    Attributes
+    ----------
+    NONE: int
+        No compression
+    BZIP2: int
+        The bzip2 compression
+    GZIP: int
+        The gzip compression
+    LZMA: int
+        The lzma compression
+    ZSTANDARD: int
+        The zstandard compression
+    """
+
+    NONE = ""
+    BZIP2 = "bz2"
+    GZIP = "gz"
+    LZMA = "xz"
+    ZSTANDARD = "zst"
+
+
+def tar_compression_types_for_filename_regex() -> str:
+    """Return the members of CompressionTypeEnum formatted for use in the FILENAME regular expression
+
+    Returns
+    -------
+    str
+        The members of CompressionTypeEnum formatted as an "or" concatenated string (including a leading empty match for
+        no compression)
+    """
+
+    return r"|".join([type_.value for type_ in CompressionTypeEnum]).replace("|", "|.")
