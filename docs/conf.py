276ead2ddaff9370a63d5ae18d1a0361c242ae80
David Runge <dvzrv@archlinux.org>
2022-09-12 17:14:02 +0200

    Set release version used by sphinx to latest
    docs/conf.py:
    As we are only targeting the latest changes on default branch, we do not
    target a specific version with the documentation. Setting release to
    "latest" is therefore more appropriate.

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2022, David Runge"
 author = "David Runge"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "latest"
 
 
 # -- General configuration ---------------------------------------------------
