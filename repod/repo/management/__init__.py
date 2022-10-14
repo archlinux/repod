dac4d724a78b185bd51de6a7ec718f7516794a43
David Runge <dvzrv@archlinux.org>
2022-10-14 17:30:50 +0200

    Remove ORJSON_OPTION from repod.repo.management
    repod/repo/management/__init__.py:
    The source of truth for `ORJSON_OPTION` is now in
    `repod.config.defaults`, so we no longer require it in
    `repod.repo.management`.

--- repod/repo/management/__init__.py
+++ repod/repo/management/__init__.py
@@ -1,5 +1,3 @@
-from orjson import OPT_APPEND_NEWLINE, OPT_INDENT_2, OPT_SORT_KEYS
-
 from repod.repo.management.outputpackage import (  # noqa: F401
     Files,
     OutputBuildInfo,
@@ -8,5 +6,3 @@ from repod.repo.management.outputpackage import (  # noqa: F401
     PackageDesc,
     export_schemas,
 )
-
-ORJSON_OPTION = OPT_INDENT_2 | OPT_APPEND_NEWLINE | OPT_SORT_KEYS
