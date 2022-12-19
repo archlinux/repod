3eee14e70521fc8f827dbed0f4c9ae2cfcfd5632
David Runge <dvzrv@archlinux.org>
2022-12-19 23:07:46 +0100

    Add favicon to HTML documentation
    docs/conf.py:
    Add favicon to HTML documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -58,6 +58,7 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 html_logo = "artwork/logo.png"
+html_favicon = "artwork/logo.ico"
 
 html_context = {
     "display_gitlab": True,
